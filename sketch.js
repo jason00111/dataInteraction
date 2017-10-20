b9efe4dea4b1a175f82e97b6c5c516e22d5da562
jason00111 <emberleyjason@gmail.com>
2017-10-19 20:08:07 -0700

    adds line visualization of amount

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 console.log('theUSD (unixtime,price,amount):', theUSD)
 
 const LEFT = 10
+const TOP = 10
 
 function setup() {
   createCanvas(500, 500)
 }
 
 function draw() {
-  theUSD.forEach((point, index) =>
-    drawPoint(index * 10 + LEFT, Number(point[1]) * 10)
-  )
+  theUSD.forEach((point, index) => {
+    const horizontal = index * 10 + LEFT
+    drawPoint(horizontal, Number(point[1]) * 10)
+    line(horizontal, TOP, horizontal, Number(point[2]) * 10 + TOP)
+  })
 }
 
 function drawPoint(x, y) {
