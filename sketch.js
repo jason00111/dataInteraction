ebfe6a6f89830f80f341af63c103d0bd27b50421
jason00111 <emberleyjason@gmail.com>
2017-10-21 17:58:53 -0700

    adds animation

--- sketch.js
+++ sketch.js
@@ -1,37 +1,62 @@
-const LEFT = 10
-const TOP = 10
+const CANVAS_WIDTH = 500, CANVAS_HEIGHT = 500,
+      LEFT = 10, TOP = 10
+
+let t = 0
+
+const speed=0.1, size=10
+
 
 function setup() {
-  createCanvas(500, 500)
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+  t = 0
 }
+
 
 function draw() {
 
-  // plotData([
-  //   ["1307576477","26.591000000000","1.000000000000"],
-  //   ["1307576818","28.000000000000","1.000000000000"],
-  //   ["1307576906","29.000000000000","0.880000000000"],
-  //   ["1307577066","29.999900000000","1.500000000000"],
-  //   ["1307579245","29.500000000000","1.000000000000"],
-  //   ["1307582051","30.000000000000","3.250000000000"],
-  //   ["1307584965","29.500000000000","1.000000000000"]
-  // ],
-  //   [ 'none', 'point', 'bar']
-  // )
+  background(255)
+
+  const angle = t*speed
+
+  drawPoint(
+    size * Math.cos(angle) + (CANVAS_WIDTH/2),
+    size * Math.sin(angle) + (CANVAS_HEIGHT/2)
+  )
+
+  drawPoint(
+    -size * Math.cos(angle) + (CANVAS_WIDTH/2),
+    -size * Math.sin(angle) + (CANVAS_HEIGHT/2)
+  )
+
+  t++
+
+
+
 
   plotData([
-      [2, 3, 2.7, 3.2, 4.1],
-      [10, 14, 12, 15, 20]
+    ["1307576477","26.591000000000","1.000000000000"],
+    ["1307576818","28.000000000000","1.000000000000"],
+    ["1307576906","29.000000000000","0.880000000000"],
+    ["1307577066","29.999900000000","1.500000000000"],
+    ["1307579245","29.500000000000","1.000000000000"],
+    ["1307582051","30.000000000000","3.250000000000"],
+    ["1307584965","29.500000000000","1.000000000000"]
   ],
-    'timeSeries'
+    [ 'none', 'point', 'bar']
   )
+
+  // plotData([
+  //     [2, 3, 2.7, 3.2, 4.1],
+  //     [10, 14, 12, 15, 20]
+  // ],
+  //   'timeSeries'
+  // )
 
   // plotData([2, 3, 2.7, 3.2, 4.1], 'timeSeries')
 }
 
 function drawPoint(x, y) {
-  const r = 5
-  ellipse(x, y, r, r)
+  ellipse(x, y, 5, 5)
 }
 
 function drawTick(point, horizontal, format) {
